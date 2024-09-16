1d15c23dfd3cc161b0cbd56b53b74ed69e17edc9
Tyson Starks <ta.starks3@gmail.com>
2024-09-16 13:08:59 -0700

    test test test

---
+++ Ox/Ox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ox
+//
+//  Created by Lord Starks on 9/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Ox App!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ox/Ox/OxApp.swift
@@ -0,0 +1,17 @@
+//
+//  OxApp.swift
+//  Ox
+//
+//  Created by Lord Starks on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct OxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
